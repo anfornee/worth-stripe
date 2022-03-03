cabafb3142f430c000f4535ecaafcde5b5c49f7c
anfornee <anthonyp@ichop.org>
2022-03-02 17:48:27 -0800

    I think I need to get the user info once they log in. Looking into auth stuff.

--- stripe/createPoralLink.ts
+++ stripe/createPoralLink.ts
@@ -1,16 +1,16 @@
-import firebase, { firestore } from '../firebase/firebaseClient'
-import { getFunctions } from 'firebase/functions'
+import { firestore } from '../firebase/firebaseClient'
+import { getFunctions, httpsCallable } from 'firebase/functions'
 
 export async function createPortalLink(uid: string) {
-  const functions = getFunctions(firestore.app, "us-central1")
-  console.log(functions)
+  console.log(uid)
+  const functions = getFunctions(firestore.app, 'us-central1')
 
-  const { data } = await functionRef({
-    returnUrl: window.location.origin,
-    locale: "us-central1", // Optional, defaults to "auto"
-    configuration: 'bpc_1JSEAKHYgolSBA358VNoc2Hs', // Optional ID of a portal configuration: https://stripe.com/docs/api/customer_portal/configuration
-  })
-  window.location.assign(data.url)
+  const functionRef = httpsCallable(functions, 'ext-firestore-stripe-payments-createPortalLink')
+  console.log(functionRef)
+  console.log('location origin: ', window.location.origin)
+  const { data } = await functionRef({ returnUrl: window.location.origin })
+  console.log('data: ', data)
+  // window.location.assign(data.url);
 
   return 'hello'
 }
