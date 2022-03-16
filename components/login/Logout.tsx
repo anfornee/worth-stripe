4d4a35b5b19811c19c64f3b5dde5f0e8f4b1b92d
anfornee <anthonyp@ichop.org>
2022-03-15 21:44:42 -0700

    fixed the dang footer

--- components/login/Logout.tsx
+++ components/login/Logout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { auth } from '../../firebase/firebaseClient'
 import Button from '@mui/material/Button'
 
-const Logout = ({ setMenuIsActive, setLoggedIn }) => {
+const Logout = ({ setMenuIsActive = value => value, setLoggedIn }) => {
   const handleLogoutClicked = async () => {
     await auth.signOut()
     setLoggedIn(false)
